0212ce48fb58f3e89971c2115a8c06c8c251964f
ybuchi <yabuch_ch@hotmail.com>
2022-01-24 13:15:16 -0500

    commit tree 9214082ba044d417e92d5160c6b99bbfc2fd2893

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+function writeCards (nameArray, event) {
+    let cardArray = [];
+    for (let i = 0; i < nameArray.length; i++){
+        let message = `Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`;
+        cardArray.push(message);
+        console.log(cardArray[i]);
+        debugger;
+    }
+    return cardArray;
+}
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+wrapGifts(gifts);
+writeCards(['Jonah', 'Johanna', 'Banana'], 'birthday');
+countDown(10);
